1c3ffe4f4bd4c3f84133d8c7469b86b9f54174ba
Grant Baron <granthbaron@aol.com>
2014-05-20 21:30:17 -0400

    Init Commit

---
+++ src/main/java/me/GRANTSWIM4/FStocks/Main.java
@@ -0,0 +1,33 @@
+package me.GRANTSWIM4.FStocks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+ 
+public final class Main extends JavaPlugin {
+	
+	    @Override
+	    public void onEnable() {
+	    	{
+	    	
+	    		
+	    		{
+	    			// Welcome message
+	    			getLogger().info("Opening stock market");
+
+	    		}
+	    		
+	    	}
+
+		}
+ 
+	    
+	 
+	    @Override
+	    public void onDisable() {
+	    	getLogger().info("Colseing the market");
+ 
+	    }
+	}
+	
+	
+	

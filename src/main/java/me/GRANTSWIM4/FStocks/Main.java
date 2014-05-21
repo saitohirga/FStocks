39b1e9cce1f0cfa036daa332c489a9632ba87d7d
Grant Baron <granthbaron@aol.com>
2014-05-21 09:30:25 -0400

    Reformated code

--- src/main/java/me/GRANTSWIM4/FStocks/Main.java
+++ src/main/java/me/GRANTSWIM4/FStocks/Main.java
@@ -2,33 +2,25 @@ package me.GRANTSWIM4.FStocks;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
- 
 public final class Main extends JavaPlugin {
-	
-	    @Override
-	    public void onEnable() {
-	    	{
-	    	
-	    		
-	    		{
-	    			// Welcome message
-	    			getLogger().info("Opening stock market");
-	 
 
-	    		}
-	    		
-	    	}
+	@Override
+	public void onEnable() {
+		{
+
+			{
+				// Welcome message
+				getLogger().info("Opening stock market");
+
+			}
 
 		}
- 
-	    
-	 
-	    @Override
-	    public void onDisable() {
-	    	getLogger().info("Colseing the market");
- 
-	    }
+
 	}
-	
-	
-	
+
+	@Override
+	public void onDisable() {
+		getLogger().info("Colseing the market");
+
+	}
+}

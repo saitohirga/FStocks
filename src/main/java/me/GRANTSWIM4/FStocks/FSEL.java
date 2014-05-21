754dd6bca9f9681f2f6b97b3164f88b43a2ec62a
Grant Baron <granthbaron@aol.com>
2014-05-21 12:37:47 -0400

    Formated code

--- src/main/java/me/GRANTSWIM4/FStocks/FSEL.java
+++ src/main/java/me/GRANTSWIM4/FStocks/FSEL.java
@@ -8,14 +8,13 @@ import com.massivecraft.factions.entity.BoardColls;
 import com.massivecraft.factions.entity.UPlayer;
 import com.massivecraft.mcore.ps.PS;
 
-public final class FSEL implements Listener {	
-	
+public final class FSEL implements Listener {
+
 	public boolean isFactionMember(Player player, Location loc) {
-        UPlayer p = UPlayer.get(player);
-        String f1 = p.getFactionName();
-        String f2 = BoardColls.get().getFactionAt(PS.valueOf(loc)).getName();
-        return f1.equalsIgnoreCase(f2);
-    }
-	
-	
+		UPlayer p = UPlayer.get(player);
+		String f1 = p.getFactionName();
+		String f2 = BoardColls.get().getFactionAt(PS.valueOf(loc)).getName();
+		return f1.equalsIgnoreCase(f2);
+	}
+
 }
--- src/main/java/me/GRANTSWIM4/FStocks/Main.java
+++ src/main/java/me/GRANTSWIM4/FStocks/Main.java
@@ -1,6 +1,5 @@
 package me.GRANTSWIM4.FStocks;
 
- 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -12,9 +11,8 @@ public final class Main extends JavaPlugin {
 			{
 				// Welcome message
 				getLogger().info("Opening stock market");
-//Event
-				getServer().getPluginManager().registerEvents(
-						new FSEL(), this);			
+				// Event
+				getServer().getPluginManager().registerEvents(new FSEL(), this);
 			}
 
 		}
